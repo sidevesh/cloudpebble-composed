b105cdfcd0f60b444636aec81e5111b8ef86075d
ElijahZAwesome <elijahbansley@monroe.k12.wi.us>
2019-05-27 17:38:54 -0500

    Github took a shit

--- cloudpebble/docker_start.sh
+++ cloudpebble/docker_start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sleep 1
 if [ ! -z "$RUN_WEB" ]; then
-	# Make sure the database is up to date so yeah.
+	# Make sure the database is up to date.
 	echo "Performing database migration."
 	python manage.py syncdb --noinput
 	python manage.py migrate
